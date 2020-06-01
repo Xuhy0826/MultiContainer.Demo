3df1795fee715e83783018fe8cf9d3222139cb60
宏宇 徐 <894174045@qq.com>
2020-06-01 22:59:25 +0800

    增加服务

---
+++ Demo.Order.Domain/Events/BuyerAndPaymentMethodVerifiedDomainEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Abstractions;
+
+namespace Demo.Order.Domain.Events
+{
+    public class BuyerAndPaymentMethodVerifiedDomainEvent : IDomainEvent
+    {
+    }
+}
